75c2e4d17fb44bd36bdd023f2e6c8fa6f4870100
frimpongopoku <pongofrimi@gmail.com>
2021-02-19 17:05:08 +0400

    Setup stocks form now, but no functionality yet :ok_hand:

--- app/src/main/java/com/pongo/biibisoft/donkomi_android/AllFormsContainerPage.java
+++ app/src/main/java/com/pongo/biibisoft/donkomi_android/AllFormsContainerPage.java
@@ -4,18 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class AllFormsContainerPage extends AppCompatActivity {
 
-  String FORM_FOR = Konstants.NEW_VENDOR;
+  String FORM_FOR = Konstants.NEW_STOCK;
   TextView pageName;
   ImageView backBtn, rightIcon;
-  LinearLayout  routineForm, stockForm;
-  RelativeLayout vendorForm;
+  LinearLayout  routineForm;
+  RelativeLayout vendorForm, stockForm;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +29,21 @@ public class AllFormsContainerPage extends AppCompatActivity {
 
   public void initialize() {
     String _for = getIntent().getStringExtra(Konstants.FORM_FOR) ;
-    FORM_FOR = _for != null ? _for : Konstants.NEW_VENDOR;
+    FORM_FOR = _for != null ? _for : Konstants.NEW_STOCK;
     backBtn = findViewById(R.id.back_icon);
     pageName = findViewById(R.id.page_name);
     vendorForm = findViewById(R.id.create_new_vendor_form);
     routineForm = findViewById(R.id.create_new_routine_form);
     stockForm = findViewById(R.id.create_new_stock_form);
+    setupStocksForm();
     generateForm(FORM_FOR);
 
+  }
+
+  public void setupStocksForm(){
+    Spinner ownersOfStockDropdown = findViewById(R.id.owner_of_stock_dropdown);
+    ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,Konstants.DUMMY_VENDORS);
+    ownersOfStockDropdown.setAdapter(adapter);
   }
 
   public void generateForm(String forWho) {
@@ -47,7 +56,9 @@ public class AllFormsContainerPage extends AppCompatActivity {
         vendorForm.setVisibility(View.VISIBLE);
         break;
       case Konstants.NEW_STOCK:
+//        setupStocksForm();
         stockForm.setVisibility(View.VISIBLE);
+
         break;
       default:
         break;
