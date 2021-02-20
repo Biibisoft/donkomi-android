53f004ce8d29d7636ffc94e70945dec2d1a164a0
frimpongopoku <pongofrimi@gmail.com>
2021-02-20 08:43:11 +0400

    Setup routine addition form now officialy done (UI) :ok_hand:

--- app/src/main/java/com/pongo/biibisoft/donkomi_android/AllFormsContainerPage.java
+++ app/src/main/java/com/pongo/biibisoft/donkomi_android/AllFormsContainerPage.java
@@ -16,8 +16,8 @@ public class AllFormsContainerPage extends AppCompatActivity {
   String FORM_FOR = Konstants.NEW_STOCK;
   TextView pageName;
   ImageView backBtn, rightIcon;
-  LinearLayout  routineForm;
-  RelativeLayout vendorForm, stockForm;
+//  LinearLayout routineForm;
+  RelativeLayout vendorForm, stockForm, routineForm;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -28,22 +28,32 @@ public class AllFormsContainerPage extends AppCompatActivity {
 
 
   public void initialize() {
-    String _for = getIntent().getStringExtra(Konstants.FORM_FOR) ;
-    FORM_FOR = _for != null ? _for : Konstants.NEW_STOCK;
+    String _for = getIntent().getStringExtra(Konstants.FORM_FOR);
+    FORM_FOR = _for != null ? _for : Konstants.NEW_ROUTINE;
     backBtn = findViewById(R.id.back_icon);
     pageName = findViewById(R.id.page_name);
     vendorForm = findViewById(R.id.create_new_vendor_form);
     routineForm = findViewById(R.id.create_new_routine_form);
     stockForm = findViewById(R.id.create_new_stock_form);
     setupStocksForm();
+    setupRoutineForm();
     generateForm(FORM_FOR);
 
   }
 
-  public void setupStocksForm(){
+  public void setupStocksForm() {
     Spinner ownersOfStockDropdown = findViewById(R.id.owner_of_stock_dropdown);
-    ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,Konstants.DUMMY_VENDORS);
+    ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Konstants.DUMMY_VENDORS);
     ownersOfStockDropdown.setAdapter(adapter);
+    pageName.setText("Create New Stock");
+  }
+
+  public void setupRoutineForm() {
+    Spinner ownersOfStockDropdown = findViewById(R.id.routine_form_vendors_dropdown);
+    ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Konstants.DUMMY_VENDORS);
+    ownersOfStockDropdown.setAdapter(adapter);
+    pageName.setText("Create New Routine");
+
   }
 
   public void generateForm(String forWho) {
@@ -56,7 +66,6 @@ public class AllFormsContainerPage extends AppCompatActivity {
         vendorForm.setVisibility(View.VISIBLE);
         break;
       case Konstants.NEW_STOCK:
-//        setupStocksForm();
         stockForm.setVisibility(View.VISIBLE);
 
         break;
